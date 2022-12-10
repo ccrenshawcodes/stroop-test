f5aad62501367dc70a95c49764f90f8d03405e98
ccrenshawcodes <55335367+ccrenshawcodes@users.noreply.github.com>
2022-12-10 10:45:39 -0700

    Update stroop.js
    Added reset button. Polished a few loose ends. Added more descriptive notes

--- stroop test/stroop.js
+++ stroop test/stroop.js
@@ -1,25 +1,45 @@
 //creating our color words and values
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
-    const colorval = ['#f7020b', '#f75802', '#f7df02', '#2bf702', '#028df7', '#7d02f7', '#f54cc2'];
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+const colorval = ['#f7020b', '#f75802', '#f7df02', '#2bf702', '#028df7', '#7d02f7', '#f54cc2'];
 
-//still need to give first word and color value
+//variables - timer
+let seconds = 30;
+let timer;
+let goBtn = document.getElementById('start');
+let timeDisplay = document.getElementById('stopwatch');
 
-//---timer---
-    let seconds = 30;
-    let timer;
-    let goBtn = document.getElementById('start');
-    let timeDisplay = document.getElementById('stopwatch');
+//variables - color word and input
+let testWord = document.getElementById('word');
+let form = document.getElementById('input');
+let newWord;
+let newColor;
 
-    let testWord = document.getElementById('word');
-    let form = document.getElementById('input');
+//variables - score
+let wordCount = 0;
+let scoreDisplay = document.getElementById('score');
 
-    let wordCount = 0;
-    let scoreDisplay = document.getElementById('score');
 
-    let newWord;
-    let newColor;
+// -- switching the words and colors --
+form.disabled = true; //you can only type when the timer is going
 
-//does the countdown
+//displays a new color word in a new color each time it gets called
+const wordSwitch = () => {
+    newWord = colors[Math.floor(Math.random() * colors.length)];
+    newColor = colorval[Math.floor(Math.random() * colorval.length)];
+    testWord.innerHTML = newWord;
+    testWord.style.color = newColor;
+}
+
+//triggers wordSwitch() above; counts entries; clears form
+form.addEventListener('keydown', function(k) {
+    if(k.code == "Enter") {
+        wordSwitch();
+        wordCount++;
+        form.value = "";
+    }
+})
+
+//Countdown timer
 function startTime() {
     function countdown() {
         seconds--;
@@ -32,7 +52,7 @@ function startTime() {
         if(seconds <= 0){
             clearInterval(timer);
             form.disabled = true;
-            scoreDisplay.innerHTML += wordCount;
+            scoreDisplay.innerHTML = `You got through ${wordCount} words!`;
             form.value = "";
         }
     };
@@ -42,34 +62,37 @@ function startTime() {
   }
 }
 
-goBtn.addEventListener('click', function() {//calls the function on start btn click
+//--end timer--
+
+//starts the timer on click; enables the form; gives 1st word
+goBtn.addEventListener('click', function() {
+  timeDisplay.innerHTML = '0:30';
   startTime();
   form.disabled = false;
+  wordSwitch();
+  wordCount = 0;
+  scoreDisplay.innerHTML = '';
 });
-//---end timer code---
 
+//reset button - stops timer, changes time display, clears word count/score display,
+//displays 'ready?' in default color, clears form
+function resetPage() {
+    wordCount = 0;
+    scoreDisplay.innerHTML = '';
 
-//---switching the words and colors!--
+    testWord.innerHTML = 'ready?';
+    testWord.style.color = '#232';
 
-form.disabled = true;
+    clearInterval(timer);
+    timeDisplay.innerHTML = '0:30';
 
-const wordSwitch = () => {
-    newWord = colors[Math.floor(Math.random() * colors.length)];
-    newColor = colorval[Math.floor(Math.random() * colorval.length)];
-    testWord.innerHTML = newWord;
-    testWord.style.color = newColor;
+    form.value = '';
+    form.disabled = true;
+    
 }
 
-form.addEventListener('keydown', function(k) {
-    if(k.code == "Enter") {
-        wordSwitch();
-        wordCount++;
-        form.value = "";
-    }
+//calls resetPage function on 'reset' button click
+document.getElementById('reset').addEventListener('click', function(){
+    resetPage();
 })
-
-//timer should:
-    //gray out or hide the text box (and start button?) when it hits 1:00:00 1 minute.
-    //display "you got through x words!"
-
 
